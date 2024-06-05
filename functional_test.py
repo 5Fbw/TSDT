ba8baa860c23e3fdf6d9ea730d2c9a5e9d5b2a51
5Fbw <565826372@qq.com>
2024-06-05 15:17:16 +0800

    massage 2: first functional_test

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
 import time
+import unittest
 # driver = webdriver.Chrome(executable_path='D:\\python3.7.7\\chromedriver_win32\\chromedriver.exe')
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-time.sleep(1000)
-assert 'Django' in browser.page_source
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
+        # time.sleep(1000)
+        self.assertIn('To-Do' , self.browser.title) , "Browser title was:" + self.browser.title
+        self.fail('Finish the test')
+
+if __name__ == "__main__":
+    unittest.main()
+
