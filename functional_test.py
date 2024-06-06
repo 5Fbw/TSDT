4ee577fa84360e94f8007116c8dacbad2874b387
5Fbw <565826372@qq.com>
2024-06-06 13:40:02 +0800

    message 6 : Fuctional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import unittest
+
+
 # driver = webdriver.Chrome(executable_path='D:\\python3.7.7\\chromedriver_win32\\chromedriver.exe')
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -11,8 +15,32 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         time.sleep(1)
-        self.assertIn('To-Do' , self.browser.title) , "Browser title was:" + self.browser.title
+
+        #检测标题和头部包含'To-Do'
+        self.assertIn('To-Do' , self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do', header_text)
+
+        #检查有输入代办项的文本框
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #文本框输入'Buy flowers'
+        inputbox.send_keys('Buy flowers')
         self.fail('Finish the test')
+        #按回车，页面更新
+        #待办事项显示'1：Buy flowers'
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_element(By.TAG_NAME,'tr')
+        self.assertIn('1：Buy flowers',[row.text for row in rows])
+
+        self.fail("Finish the test!")
 
 if __name__ == "__main__":
     unittest.main()
