264d46d36a836b1c4fb8636139fa9f87523ef884
5Fbw <565826372@qq.com>
2024-06-11 11:04:30 +0800

    message 15 : new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -36,18 +36,18 @@ class HomePageTest(TestCase):
         # self.assertIn('A new list item',response.content.decode())
         # self.assertTemplateUsed(response, 'home.html')
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-new-page/')
 
     def test_only_save_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
 
-    def test_display_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-        response = self.client.get('/')
-        self.assertIn('item 1',response.content.decode())
-        self.assertIn('item 2', response.content.decode())
+    # def test_display_all_list_items(self):
+    #     Item.objects.create(text='item 1')
+    #     Item.objects.create(text='item 2')
+    #     response = self.client.get('/')
+    #     self.assertIn('item 1',response.content.decode())
+    #     self.assertIn('item 2', response.content.decode())
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -65,3 +65,16 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,'The First list item')
         self.assertEqual(second_saved_item.text,'Item the second')
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response =  self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response,'list.html')
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-new-page/')
+
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,14 @@ from lists.models import Item
 # home_page = None
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text','')
-        Item.objects.create(text=new_item_text)
-        return redirect('/')
-    else:
-        new_item_text = ''
+        # new_item_text = request.POST.get('item_text','')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-new-page/')
+    return render(request, 'home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request,'home.html',{
+    return render(request, 'list.html', {
         'items': items
     })
 
