96b2fc75bcaba74e991f5594956c6e41be157e43
5Fbw <565826372@qq.com>
2024-06-11 10:10:03 +0800

    message 13 : refactor sleep()

--- functional_test/test.py
+++ functional_test/test.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 from django.test import LiveServerTestCase
 
-
+MAX_WAIT = 10
 # driver = webdriver.Chrome(executable_path='D:\\python3.7.7\\chromedriver_win32\\chromedriver.exe')
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -13,10 +14,23 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    # def check_for_row_in_list_table(self,row_text):
+    #     table = self.browser.find_element(By.ID, 'id_list_table')
+    #     rows = table.find_elements(By.TAG_NAME, 'tr')
+    #     self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self,row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get(self.live_server_url)
@@ -43,10 +57,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to List')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table("1：Buy flowers")
-        self.check_for_row_in_list_table("2：Give a gift to List")
+        self.wait_for_row_in_list_table("1：Buy flowers")
+        self.wait_for_row_in_list_table("2：Give a gift to List")
         self.fail("Finish the test!")
 
 # if __name__ == "__main__":
