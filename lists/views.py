5dd0082c2dbcdb40ed2dc8cae8807e3ab0920548
5Fbw <565826372@qq.com>
2024-06-11 11:17:24 +0800

    message 16 : form URL of new list by POST

--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from lists.models import Item
 # home_page = None
 def home_page(request):
-    if request.method == 'POST':
-        # new_item_text = request.POST.get('item_text','')
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-new-page/')
+    # if request.method == 'POST':
+    #     # new_item_text = request.POST.get('item_text','')
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-new-page/')
     return render(request, 'home.html')
 
 def view_list(request):
@@ -14,5 +14,7 @@ def view_list(request):
     return render(request, 'list.html', {
         'items': items
     })
-
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
 # Create your views here.
